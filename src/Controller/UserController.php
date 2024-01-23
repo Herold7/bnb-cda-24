909999010d94f46c8b8d243e94e9960ec8380cd3
Jensone <hello@jensone.com>
2024-01-23 22:54:47 +0100

    🔧 corriger(UserController.php) : supprimer les imports inutilisés et mettre à jour les noms des services ✨ fonctionnalité(UserController.php) : mettre à jour l'appel de la méthode de mise à jour du profil avec le nouveau nom du service

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,11 +2,8 @@
 
 namespace App\Controller;
 
-use App\Entity\User;
 use App\Form\ProfileType;
-use App\Repository\UserRepository;
-use App\Service\ProfileUpdate;
-use Doctrine\ORM\EntityManager;
+use App\Service\ProfileService;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -18,14 +15,14 @@ class UserController extends AbstractController
     #[Route('/complete-profile', name: 'complete_profile')]
     public function index(
         Request $request,
-        ProfileUpdate $profileUpdate,
+        ProfileService $profileService,
         EntityManagerInterface $em
     ): Response
     {
         $form = $this->createForm(ProfileType::class);
         $form->handleRequest($request);
         if($form->isSubmitted() && $form->isValid()) {
-            $profileUpdate->updateProfile($form, $this->getUser(), $em);
+            $profileService->updateProfile($form, $this->getUser(), $em);
             
             $this->addFlash('success', 'Your profile has been updated');
             return $this->redirectToRoute('account');
