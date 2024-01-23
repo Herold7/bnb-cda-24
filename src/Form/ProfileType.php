e9ae4dc0e88e496e9b20063ccbd462e0776fd4b3
Jensone <hello@jensone.com>
2024-01-23 22:55:37 +0100

    🔧 corriger(ProfileType.php) : ajouter les types de champs manquants et les classes CSS pour les champs de formulaire 🔧 corriger(ProfileType.php) : ajouter la contrainte de taille maximale pour le champ d'image et le message d'erreur correspondant

--- src/Form/ProfileType.php
+++ src/Form/ProfileType.php
@@ -4,46 +4,80 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Validator\Constraints\File;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 
 class ProfileType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            // ->add('roles')
-            ->add('firstname')
-            ->add('lastname')
-            ->add('birthyear')
-            ->add('address')
-            ->add('city')
-            ->add('country')
-            // ->add('image', FileType::class, [
-            //     'label' => 'Profile picture',
-            //     'mapped' => false,
-            //     'required' => true,
-            //     'attr' => [
-            //         'class' => 'form-control-file'
-            //     ],
-            //     'constraints' => [
-            //         new File([
-            //             'maxSize' => '1024k',
-            //             'mimeTypes' => [
-            //                 'image/*',
-            //             ],
-            //             'mimeTypesMessage' => 'Please upload a valid image',
-            //         ])
-            //     ],
-            // ])
-            ->add('job')
-            ->add('submit', SubmitType::class, [
-                'label' => 'Save',
+            ->add('roles', ChoiceType::class, [
+                'mapped' => false,
+                'choices' => [
+                    'Traveler' => 'user',
+                    'Host' => 'host',
+                    'Both' => 'host'
+                ],
                 'attr' => [
-                    'class' => 'btn btn-custom btn-block'
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('firstname', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('lastname', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('birthyear', NumberType::class, [
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('address', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('city', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('country', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('image', FileType::class, [
+                'mapped' => false,
+                'required' => false,
+                'attr' => [
+                    'class' => 'form-control-file'
+                ],
+                'constraints' => [
+                    new File([
+                        'maxSize' => '1024k',
+                        'maxSizeMessage' => 'The file is too large ({{ size }} {{ suffix }}). Allowed maximum size is {{ limit }} {{ suffix }}.',
+                        'mimeTypes' => [
+                            'image/*',
+                        ],
+                        'mimeTypesMessage' => 'Please upload a valid image',
+                    ])
+                ],
+            ])
+            ->add('job', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control'
                 ]
             ])
         ;
