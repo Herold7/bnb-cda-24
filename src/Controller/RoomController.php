dd6b1172389af881e3d53e8bf450113017080a2f
Jensone <hello@jensone.com>
2024-03-18 09:56:32 +0100

    ♻️ refactor(RoomController.php): remove unused imports and methods, clean up code and improve readability

--- src/Controller/RoomController.php
+++ src/Controller/RoomController.php
@@ -2,13 +2,8 @@
 
 namespace App\Controller;
 
-use Stripe\Stripe;
 use App\Entity\Room;
-use App\Entity\Booking;
-use Stripe\Checkout\Session;
 use App\Repository\RoomRepository;
-use App\Repository\BookingRepository;
-use Doctrine\ORM\EntityManagerInterface;
 use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -67,74 +62,4 @@ class RoomController extends AbstractController
         ]);
     }
 
-    #[Route('/confirmation', name: 'book_room', methods: ['GET', 'POST'])]
-    public function bookRoom(
-        Request $request,
-        EntityManagerInterface $em,
-        RoomRepository $roomRepository,
-    ): Response {
-        if ($request->isMethod('POST')) { // Si la méthode est POST
-            $data = $request->request->all(); // Toutes les données du formulaire sont récupérées
-            $confirmation = new Booking(); // Crée un nouvel objet booking
-            $confirmation->setCheckIn(new \DateTime($data['checkin'])) // Objet datetime attendu
-                ->setCheckOut(new \DateTime($data['checkout'])) // Objet datetime attendu
-                ->setOccupants($data['guests']) // Nombre attendu
-                ->setTraveler($this->getUser()) // Objet user attendu
-                ->setRoom($roomRepository->find($data['room'])) // Objet room attendu
-            ;
-            $em->persist($confirmation); // Prépare l'insertion en BDD
-            $em->flush(); // Exécute l'insertion en BDD
-
-            $dateDiff = $confirmation->getCheckIn()->diff($confirmation->getCheckOut()); // Calcul de la durée du séjour
-            $totalAmount = $dateDiff->days * $confirmation->getRoom()->getPrice(); // Calcul du montant total
-
-            return $this->render('room/confirmation.html.twig', [
-                'confirmation' => $confirmation, // retourne l'objet booking à la vue
-                'dateDiff' => $dateDiff, // retourne la durée du séjour à la vue
-                'totalAmount' => $totalAmount // retourne le montant total à la vue
-            ]);
-        } else { // Si la méthode est GET
-            return $this->render('room/confirmation.html.twig'); // retourne la vue sans objet
-        }
-    }
-
-    #[Route('/payment', name: 'payment', methods: ['POST'])]
-    public function payment(
-        Request $request,
-        BookingRepository $bookingRepository,
-    ) {
-        $booking = $bookingRepository->find($request->request->get('number')); // on récupère la réservation
-        Stripe::setApiKey($this->getParameter('STRIPE_SECRET_KEY')); // on récupère la clé secrète
-
-        header('Content-Type: application/json');
-        $checkout_session = Session::create([
-            'payment_method_types' => ['card'],
-            'line_items' => [[
-                    'price_data' => [
-                        'currency' => 'eur',
-                        'unit_amount' => $request->request->get('total') * 100, // Stripe utilise des centimes
-                        'product_data' => [ // Les informations du produit sont personnalisables
-                            'name' => $request->request->get('title')],
-                        ],
-                    'quantity' => 1,
-                ]],
-                'mode' => 'payment',
-                'success_url' => $this->generateUrl('payment_success', [], 0),
-                'cancel_url' => $this->generateUrl('payment_cancel', [], 0),
-            ]);
-
-        return $this->redirect($checkout_session->url, 303);
-    }
-
-    #[Route('/payment/success', name: 'payment_success', methods: ['GET'])]
-    public function paymentSuccess(): Response
-    {
-        return $this->render('room/payment_success.html.twig');
-    }
-
-    #[Route('/payment/cancel', name: 'payment_cancel', methods: ['GET'])]
-    public function paymentCancel(): Response
-    {
-        return $this->render('room/payment_cancel.html.twig');
-    }
 }
